f49fb65e3882017ca8e3e020e6d92111c080eb1f
hang-1n-there <s22037@gsm.hs.kr>
2023-03-30 22:53:33 +0900

    model.py

---
+++ pytorch_ignight/model.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+
+class ImgClassifier(nn.Module):
+    def __init__(self, input_size, output_size):
+        self.input_size = input_size
+        self.output_size = output_size
+        
+        super().__init__()
+        
+        self.layers = nn.Sequential(
+            nn.Linear(input_size, 500),
+            nn.ReLU(),
+            nn.BatchNorm1d(500),
+            nn.Linear(500, 200),
+            nn.ReLU(),
+            nn.BatchNorm1d(200),
+            nn.Linear(200, 100),
+            nn.ReLU(),
+            nn.BatchNorm1d(100),
+            nn.Linear(100, 50),
+            nn.ReLU(),
+            nn.BatchNorm1d(50),
+            nn.Linear(50,20),
+            nn.ReLU(),
+            nn.BatchNorm1d(20),
+            nn.Linear(20,10),
+            nn.ReLU(),
+            nn.BatchNorm1d(10),
+            nn.Linear(10,output_size)
+        )
+        
+    def forward(self, x):
+        y = self.layers(x)
+        
+        return y
